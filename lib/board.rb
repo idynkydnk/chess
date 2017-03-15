7bebc9ed1013af1da597192543ffadd46242c8dc
Kyle Thomson <idynkydnk@gmail.com>
2017-03-15 14:14:34 -0400

    board can now place a piece

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,8 @@ class Board
   def initialize
     @grid = Array.new(8) { Array.new(8) { " " }}
   end
+
+  def place_piece(col, row, piece)
+    @grid[col][row] = piece
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,5 +6,21 @@ describe Board do
       board = Board.new
       expect(board.grid.length).to be(8)
     end
+     
+    it "sets 8 colums" do
+      board = Board.new
+      board.grid.each do |row|
+        expect(row.length).to be(8)
+      end
+    end
+  end
+
+  describe ".place_piece" do
+    it "places a piece on the grid A-H,1-8" do
+      board = Board.new
+      object = double("object")
+      board.place_piece(0, 0, object)
+      expect(board.grid[0][0]).to eql(object)
+    end
   end
 end
