8da2f60c6e4d94642e5bce4db44ccd6a023ccaba
Kyle Thomson <idynkydnk@gmail.com>
2017-03-17 14:07:01 -0400

    possible moves calculates left queen moves

--- lib/queen.rb
+++ lib/queen.rb
@@ -10,12 +10,21 @@ class Queen < GamePiece
 
   def calc_possible_moves
     right_moves
+    left_moves
   end
 
   def right_moves
     x = 1
     while location_valid?([@position[0] + x, @position[1]])
       @possible_moves << [@position[0] + x, @position[1]]
+      x += 1
+    end
+  end
+
+  def left_moves
+    x = 1
+    while location_valid?([@position[0] - x, @position[1]])
+      @possible_moves << [@position[0] - x, @position[1]]
       x += 1
     end
   end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -2,11 +2,18 @@ require "queen"
 
 describe Queen do
   context "should have possible moves" do
-    it "should have move 0,1" do
+    it "should check all moves right horizontal" do
       x = Queen.new
       expect(x.possible_moves).to include([1,0])
       expect(x.possible_moves).to include([7,0])
       expect(x.possible_moves).not_to include([8,0])
+    end
+
+    it "should check left moves" do
+      x = Queen.new
+      expect(x.possible_moves).not_to include([-1,0])
+      expect(x.possible_moves).not_to include([0,0])
+
     end
   end
 end
