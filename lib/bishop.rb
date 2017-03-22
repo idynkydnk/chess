cc15b0bbde844a67849545518e7def651a971817
Kyle Thomson <idynkydnk@gmail.com>
2017-03-22 15:04:10 -0400

    bishop class complete

---
+++ lib/bishop.rb
@@ -0,0 +1,60 @@
+require_relative "game_piece"
+
+class Bishop < GamePiece
+
+  def initialize(position = [0,0])
+    @position = position
+    @possible_moves = [] 
+    calc_possible_moves
+  end
+
+  def position=(loc)
+    if location_valid?(loc)
+      @possible_moves = []
+      @position = loc
+      calc_possible_moves
+    end
+  end
+
+  def calc_possible_moves
+    right_moves
+    left_moves
+    up_moves
+    down_moves
+  end
+  
+  private
+
+  def right_moves
+    x = 1
+    while location_valid?([@position[0] + x, @position[1]])
+      @possible_moves << [@position[0] + x, @position[1]]
+      x += 1
+    end
+  end
+
+  def left_moves
+    x = 1
+    while location_valid?([@position[0] - x, @position[1]])
+      @possible_moves << [@position[0] - x, @position[1]]
+      x += 1
+    end
+  end
+
+  def up_moves
+    x = 1
+    while location_valid?([@position[0], @position[1] + x])
+      @possible_moves << [@position[0], @position[1] + x]
+      x += 1
+    end
+  end
+
+  def down_moves
+    x = 1
+    while location_valid?([@position[0], @position[1] - x])
+      @possible_moves << [@position[0], @position[1] - x]
+      x += 1
+    end
+  end
+
+end
---
+++ spec/bishop_spec.rb
@@ -0,0 +1,14 @@
+require "bishop"
+
+describe Bishop do
+  context "should have possible moves" do
+    it "should check all moves" do
+      x = Bishop.new
+      x.position = [1,3]
+      expect(x.possible_moves).to include([1,7])
+      expect(x.possible_moves).to include([3,3])
+      expect(x.possible_moves).not_to include([8,0])
+      expect(x.possible_moves).not_to include([2,5])
+    end
+  end
+end
