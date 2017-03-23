daaaae2f330b6d196bb0476d668b923224c22c60
Kyle Thomson <idynkydnk@gmail.com>
2017-03-23 11:31:28 -0400

    fixed indentation

--- lib/game_piece.rb
+++ lib/game_piece.rb
@@ -1,14 +1,19 @@
 class GamePiece
-  attr_accessor :position, :possible_moves
+  attr_accessor :position, :possible_moves, :color
 
-  def initialize(position = [0,0])
+  def initialize(color, position = [0,0])
+    if color == "yellow" || color == "red"
+      @color = color
+    else
+      raise "game piece must be either yellow or red"
+    end
     @position = position
     @possible_moves = []
   end
 
   def location_valid?(location)
-  	if location[0] >= 0 && location[0] <= 7 &&
-    	location[1] >= 0 && location[1] <= 7
+    if location[0] >= 0 && location[0] <= 7 &&
+      location[1] >= 0 && location[1] <= 7
       return true
     else
       return false
---
+++ lib/pawn.rb
@@ -0,0 +1,78 @@
+require_relative "game_piece"
+
+class Pawn < GamePiece
+
+  def initialize(position = [0,0])
+    @position = position
+    @possible_moves = [] 
+    calc_possible_moves
+  end
+
+  def position=(loc)
+    if location_valid?(loc)
+      @possible_moves = []
+      @position = loc
+      calc_possible_moves
+    end
+  end
+
+  def calc_possible_moves
+    up_moves
+    down_moves
+    diagonal_up
+    diagonal_down
+    diagonal_left_up
+    diagonal_left_down
+  end
+  
+  private
+
+  def right_moves
+    if location_valid?([@position[0] + 1, @position[1]])
+      @possible_moves << [@position[0] + 1, @position[1]]
+    end
+  end
+
+  def left_moves
+    if location_valid?([@position[0] - 1, @position[1]])
+      @possible_moves << [@position[0] - 1, @position[1]]
+    end
+  end
+
+  def up_moves
+    if location_valid?([@position[0], @position[1] + 1])
+      @possible_moves << [@position[0], @position[1] + 1]
+    end
+  end
+
+  def down_moves
+    if location_valid?([@position[0], @position[1] - 1])
+      @possible_moves << [@position[0], @position[1] - 1]
+    end
+  end
+
+  def diagonal_up
+    if location_valid?([@position[0] + 1, @position[1] + 1])
+      @possible_moves << [@position[0] + 1, @position[1] + 1]
+    end
+  end
+
+  def diagonal_down
+    if location_valid?([@position[0] + 1, @position[1] - 1])
+      @possible_moves << [@position[0] + 1, @position[1] - 1]
+    end
+  end
+
+  def diagonal_left_up
+    if location_valid?([@position[0] - 1, @position[1] + 1])
+      @possible_moves << [@position[0] - 1, @position[1] + 1]
+    end
+  end
+
+  def diagonal_left_down
+    if location_valid?([@position[0] - 1, @position[1] - 1])
+      @possible_moves << [@position[0] - 1, @position[1] - 1]
+    end
+  end
+
+end
